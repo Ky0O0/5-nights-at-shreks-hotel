7963d1c8c88769a138e78a8fdb6cea5591086a72
Ky000 <banana11122@wp.pl>
2023-03-03 19:28:32 +0100

    Add files via upload

---
+++ 5NightsAtShreksHotel/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="You don't want to mess with Grek... but he does!">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image/png" href="icon/icon.png">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>5 Nights At Grek's Hotel (Shrek is fired) by RenderPi</title>
+</head>
+<body>
+	<div id="container">
+		<div id="image_div">
+			<p style="margin: 0;">
+				<img src="images/yHNJkt.png" class="yHNJkt_img">
+			</p>
+		</div>
+		<div id="left_sec">
+			<p>A downloadable game for Windows, macOS, and Linux</p>
+			<div id="buy_div1">
+				<a href="https://renderpi.itch.io/5-nights-at-shreks-hotel" target="_blank"><button class="buy_btn">Buy Now</button></a>
+				<h3 style="float: left; margin: 0; margin-left: 5px; margin-top: 3px;">$4.99 USD</h3>
+				<p style="float: left; margin: 0; margin-left: 5px; margin-top: 5px; opacity: 0.5;">or more</p>
+			</div>
+			<div id="content">
+				<iframe width="520" height="300" src="https://www.youtube.com/embed/4-Q0wNZo4YE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+				<p>5 Nights At Grek's Hotel | Full Version</p>
+				<p>Shrek is fired due to copyright</p>
+				<h3>ABOUT GAME</h3>
+				<p style="text-align: left;">You have sold your house and moved to a new city to live in. You have a job but don't have enough money to buy your new home yet. So you decided to go to Grek's hotel for five nights, so maybe you could afford the money you needed. The hotel manager is Grek ... but something is wrong with him ...</p>
+				<p>
+					<img src="gif/90z3J_.gif">	
+				</p>
+				<p>WARNING! This horror game includes load jumpscares and flashing lights. OBVIOUSLY, IT'S A HORROR GAME. If you are scared of Grek please don't play this game...but you can still buy it.</p>
+				<em>If you enjoyed the game, please don’t forget to write down your opinion in the comment below; your support will help me develop the game further!</em>
+				<p>This game is the second indie horror game I made. Let me know what you think about the game in the comments, and please report any bugs or lags. Thank you.</p>
+				<h2>Have you seen our new game?</h2>
+				<p>Check it out, <a href="https://renderpi.itch.io/the-true-ingredients" style="color: #41B060;" target="_blank">here, Grek might get involved </a>:)</p><br>
+				<h3>FOLLOW US</h3>
+				<p>Website: <a href="https://www.renderpi.com/" style="color: #41B060;" target="_blank">renderpi.com</a></p>
+				<p>Youtube: <a href="https://www.youtube.com/@renderpigames" style="color: #41B060;" target="_blank">https://www.youtube.com/@renderpigames</a></p>
+				<p>Discord ID: renderpi#3867</p>
+				<p>Discord Server: <a href="https://discord.gg/hBzMCQ8ar4" style="color: #41B060;" target="_blank">https://discord.gg/hBzMCQ8ar4</a></p>
+				<p>Email: renderpi@outlook.com</p>
+				<p>Other: <a href="https://linktr.ee/renderpi" style="color: #41B060;" target="_blank">https://linktr.ee/renderpi</a></p>
+				<p>If you want to be a volunteer for testing out our games please subscribe on our website and send us a message with the spec of your PC or Mac</p>
+			</div>
+			<h2>Purchase</h2>
+			<div id="buy_div2">
+				<a href="https://renderpi.itch.io/5-nights-at-shreks-hotel" target="_blank"><button class="buy_btn">Buy Now</button></a>
+				<h3 style="float: left; margin: 0; margin-left: 5px; margin-top: 3px;">$4.99 USD</h3>
+				<p style="float: left; margin: 0; margin-left: 5px; margin-top: 5px; opacity: 0.5;">or more</p>
+			</div>
+			<p>In order to download this game you must purchase it at or above the minimum price of $4.99 USD. You will get access to the following files:</p>
+			<div id="windows">
+				<p style="float: left; margin: 0;" title="5-Night-At-Greks-Hotel - Win.zip"><b>5-Night-At-Greks-Hotel - Win.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">199 MB<i style="margin-left: 5px;" class="fa fa-windows"></i></p>
+			</div><br>
+			<div id="apple">
+				<p style="float: left; margin: 0;" title="5-Night-At-Greks-Hotel - macOS.zip"><b>5-Night-At-Greks-Hotel - macOS.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">212 MB<i style="margin-left: 5px;" class="fa fa-apple"></i></p>
+			</div><br>
+			<div id="linux">
+				<p style="float: left; margin: 0;" title="5-Night-At-Greks-Hotel - Linux.zip"><b>5-Night-At-Greks-Hotel - Linux.zip</b></p>
+				<p style="float: left; margin: 0; opacity: .5; margin-left: 5px;">205 MB<i style="margin-left: 5px;" class="fa fa-apple"></i></p>
+			</div>
+			<h2>Development log</h2>
+			<div id="log1">
+				<a style="color: #41B060; float: left;" href="https://renderpi.itch.io/5-nights-at-shreks-hotel/devlog/398562/say-hello-to-grek-grek-is-love" target="_blank">Say hello to Grek, Grek is Love</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Jun 29, 2022</p>
+			</div>
+			<div id="log2">
+				<a style="color: #41B060; float: left;" href="https://renderpi.itch.io/5-nights-at-shreks-hotel/devlog/384168/new-builds" target="_blank">New builds</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">May 23, 2022</p>
+			</div>
+			<div id="log3">
+				<a style="color: #41B060; float: left;" href="https://renderpi.itch.io/5-nights-at-shreks-hotel/devlog/368826/5-nights-at-shreks-hotel" target="_blank">5 Nights At Shrek's Hotel</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Apr 12, 2022</p>
+			</div>
+			<div id="log4">
+				<a style="color: #41B060; float: left;" href="https://renderpi.itch.io/5-nights-at-shreks-hotel/devlog/361841/one-night-at-shreks-hotel" target="_blank">One Night At Shrek's Hotel</a>
+				<p style="float: left; margin: 0; margin-left: 5px; opacity: .5;">Mar 25, 2022</p>
+			</div>
+			<h2>Comments</h2>
+			<div id="comments_div">
+				<form action="index.php" method="POST">
+					<input type="text" name="content" class="input_text" placeholder="Write your comment...">
+					<input type="submit" class="input_submit" value="Post comment" name="submit">
+				</form>
+				<?php
+					if (isset($_POST['content']) && $_POST['content'] !=""){
+						$content = $_POST['content'];
+						$base = mysqli_connect("localhost", "root", "", "shrek");
+						$que1 = "INSERT INTO data VALUES('1', '$content')";
+						mysqli_query($base, $que1);
+						mysqli_close($base);
+					}
+				?>
+				<br>
+				<div id="comments_div2">
+					<?php
+						$base = mysqli_connect("localhost", "root", "", "shrek");
+						$que2 = "SELECT * FROM data";
+						$result = mysqli_query($base, $que2);
+						while($a = mysqli_fetch_array($result)){
+							echo "<img src='images/frog.png'> `Your Nick`<br>".$a['content']."<br><br>";
+						}
+						mysqli_close($base);
+					?>
+				</div>
+			</div>
+		</div>
+		<div id="right_sec">
+			<p>
+				<img src="gif/a9Lz0e.gif" width="330">
+			</p>
+			<p>
+				<img src="images/YORz9z.png" width="330">
+			</p>
+			<p>
+				<img src="images/laZBL3.png" width="330">
+			</p>
+			<p>
+				<img src="images/vbi7hF.png" width="330">
+			</p>
+			<p>
+				<img src="images/Ofe63g.png" width="330">
+			</p>
+			<p>
+				<img src="images/jGJOlO.png" width="330">
+			</p>
+			<p>
+				<img src="images/8rKDvK.png" width="330">
+			</p>
+		</div>
+	</div>
+</body>
+</html>
